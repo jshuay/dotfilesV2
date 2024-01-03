eef29746b716fdfb7d76856716d34059e9478640
jshuay <jshuay@gmail.com>
2024-01-03 10:54:47 -0800

    Change nvim leap keymap

--- nvim/lua/plugins/leap.lua
+++ nvim/lua/plugins/leap.lua
@@ -1,6 +1,10 @@
 return {
     "ggandor/leap.nvim",
     config = function()
+        local leap = require("leap")
+        leap.opts.highlight_unlabeled_phase_one_targets = true
+        leap.opts.max_phase_one_targets = 0
+
         -- Searching in all windows (including the current one) on the tab page.
         LEAP = function()
             require("leap").leap({
@@ -12,6 +16,6 @@ return {
                 )
             })
         end
-        require("keymap").set({ "n", "v" }, "<LEADER><LEADER>", "<CMD>lua LEAP()<CR>")
+        require("keymap").set({ "n", "v" }, "?", "<CMD>lua LEAP()<CR>")
     end
 }
